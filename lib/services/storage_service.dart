4c350485262e4ecf41f5684eb0460703c70f8028
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-19 15:07:42 -0300

    feat: storage service class

---
+++ lib/services/storage_service.dart
@@ -0,0 +1,18 @@
+import "dart:convert";
+import "package:shared_preferences/shared_preferences.dart";
+import "../models/task.dart";
+
+class StorageService {
+  static const String _key = 'zen_tasks';
+  final SharedPreferences _prefs;
+
+  StorageService(this._prefs);
+
+  Future<List> getTasks() async {
+    final String? tasksString = _prefs.getString(_key);
+    if (tasksString == null) return [];
+
+    final List decodedList = json.decode(tasksString);
+    return decodedList.map((item) => Task.fromJson(item)).toList();
+  }
+}
