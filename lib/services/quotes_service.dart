2f573184cc29df3e8bb8be6fe7f4ee3a5df03614
Lucas Valdez <lnvaldez24@gmail.com>
2025-01-20 13:42:21 -0300

    feat: get random quote from api

--- lib/services/quotes_service.dart
+++ lib/services/quotes_service.dart
@@ -3,17 +3,17 @@ import "dart:convert";
 import "../models/quote.dart";
 
 class QuotesService {
-  static const String _baseUrl = 'https://zenquotes.io/api/random';
+  final _baseUrl = Uri.parse('https://zenquotes.io/api/random');
 
   Future getRandomQuote() async {
     try {
-      final response = await http.get(Uri.parse(_baseUrl));
+      final response = await http.get(_baseUrl);
 
       if (response.statusCode == 200) {
         final List data = json.decode(response.body);
         return Quote.fromJson(data[0]);
       } else {
-        throw Exception('Failed to load quote');
+        throw Exception('Failed to load quote: ${response.statusCode}');
       }
     } catch (e) {
       throw Exception('Failed to load quote: $e');
